b6241712c158befcf7310cb5b7190d92ae6cd5bc
Valtercio Santiago Ferreira Junior <valtercio.j@academico.ifrn.edu.br>
2020-02-14 17:55:44 -0300

    criacao de programa de adivinhacao

---
+++ README.md
@@ -0,0 +1,9 @@
+# Requerimento: python3 e pip ou pip3
+
+# Modo de uso: python adivinhando_numero.py ou python adivinhando_numero.py - Recomenda-se que rode no modo root
+
+Se não tiver as dependencias instalado irá instalar altomaticamente.
+
+Como Brincar: Voce irá chutar um numero e o programa irá adivinhar qual numero voce chutou.
+
+Bom Divertimento.
---
+++ adivinhando_numero.py
@@ -0,0 +1,42 @@
+import sys,os
+from random import randint
+plat = sys.platform
+try:
+    import PySimpleGUI as sg
+    from googlesearch import search
+except:
+    if plat == 'win32':
+        os.system('pip install pysimplegui')
+        import PySimpleGUI as sg
+    elif plat == 'linux':
+        os.system('pip3 install pysimplegui')
+        import PySimpleGUI as sg
+aleatorio = randint(0,100)
+
+class telapython:
+  def __init__(self):
+    sg.change_look_and_feel('DarkTeal7')
+    layout = [
+      [sg.Text('Chute um numero de 0 a 100',size=(12,0)),sg.Input(size=(10,10),key='numero')],
+      [sg.Button('enviar')],
+      [sg.Output(size=(30,10))]
+      
+    ]
+    self.janela = sg.Window('Dados do usuario').layout(layout)
+
+  def iniciar(self):
+    while True:
+      self.Button, self.values = self.janela.Read()
+      num = int(self.values['numero'])
+      if aleatorio == num:
+          print('Parabens, voce acertou!!')         
+      else:
+          if num > aleatorio:
+              print()
+              print('Chute um numero menor')
+          else:
+              print()
+              print('Chute um valor maior')
+
+tela = telapython()
+tela.iniciar()
